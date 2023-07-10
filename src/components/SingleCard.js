eafd88187bf8575770149a0d563012c203c549b9
Shaheer Mansoor <shaheermansoor321@gmail.com>
2023-07-09 18:31:23 -0700

    Developed the whole logic

---
+++ src/components/SingleCard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./SingleCard.css"
+
+export default function SingleCard({ card, handleChoice, flipped, disabled }) {
+
+  const handleClick = () => {
+    if(!disabled){
+      handleChoice(card);
+    }
+  };
+  
+  return (
+    <div className="card">
+        <div className='flipped'>
+          <img src={card.src} className='front' alt="card front" />
+          <img 
+            className='back'
+            src="/img/cover.png"
+            alt="card back" 
+            onClick={handleClick}
+          />
+        </div>
+    </div>
+  )
+}
+
+// {flipped ? 'flipped' : ""}
